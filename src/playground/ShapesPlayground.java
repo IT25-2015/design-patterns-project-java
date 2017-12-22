abcb203a094c84b9e3fb3c6ae0f36398d417480c
Aleksandar Babic <alekski64@icloud.com>
2017-12-22 15:00:27 +0100

    Removed unused playground code

--- src/playground/ShapesPlayground.java
+++ src/playground/ShapesPlayground.java
@@ -7,10 +7,7 @@ import shapes.point.Point;
 public class ShapesPlayground {
 
 	public static void main(String[] args) {
-		Point pt = new Point(1, 2, Color.BLACK);
-		pt.setX(2);
-		System.out.println(pt.getX());
-		System.out.println(pt);
+		
 	}
 
 }
