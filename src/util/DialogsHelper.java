78716a98b314e986156c3e9b7c2747f02fb7a872
Aleksandar Babic <alekski64@icloud.com>
2017-12-25 10:40:15 +0100

    Added JOptionPane helper class

--- src/util/DialogsHelper.java
+++ src/util/DialogsHelper.java
@@ -1,5 +1,57 @@
 package util;
 
+import javax.swing.JOptionPane;
+
 public class DialogsHelper {
+	/**
+	 * Will open JOptionPane with ERROR_MESSAGE icon and given string as error
+	 * message
+	 * 
+	 * @param message
+	 */
+	public static void showErrorMessage(String message) {
+		JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
+	}
+
+	/**
+	 * Will open JOptionPane with INFORMATION_MESSAGE icon and given string as
+	 * message
+	 * 
+	 * @param message
+	 */
+	public static void showInformationMessage(String message) {
+		JOptionPane.showMessageDialog(null, message, "Information", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	/**
+	 * Will open JOptionPane with PLAIN_MESSAGE icon and given string as message
+	 * 
+	 * @param message
+	 */
+	public static void showPlainsMessage(String message) {
+		JOptionPane.showMessageDialog(null, message, "Success", JOptionPane.PLAIN_MESSAGE);
+	}
+
+	/**
+	 * Will show JOptionPane confirm dialog, returns boolean based on user answer
+	 * 
+	 * @param question
+	 * @return
+	 */
+	public static boolean askUserToConfirm(String question) {
+		int response = JOptionPane.showConfirmDialog(null, question, "Confirm action", JOptionPane.YES_NO_OPTION,
+				JOptionPane.QUESTION_MESSAGE);
+		switch (response) {
+		case JOptionPane.NO_OPTION:
+			return false;
+		case JOptionPane.YES_OPTION:
+			return true;
+		case JOptionPane.CLOSED_OPTION:
+			return false;
+		default:
+			return false;
+		}
+
+	}
 
 }
